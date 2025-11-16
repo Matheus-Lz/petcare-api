ad37f7b795760d682c5a26c5af0926558f1b8f41
Matheus Luiz <matheus.privado.pb@gmail.com>
2025-11-16 15:12:54 -0300

    Utilizando variaveis para retornar texto do tipo de erro

--- src/main/java/com/petcare/petcare_api/infrastructure/handler/GlobalExceptionHandler.java
+++ src/main/java/com/petcare/petcare_api/infrastructure/handler/GlobalExceptionHandler.java
@@ -14,49 +14,49 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException ex) {
-        var errorResponse = new ErrorResponse("NOT_FOUND", ex.getMessage());
+        var errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.getReasonPhrase(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
-        var errorResponse = new ErrorResponse("BAD_REQUEST", ex.getMessage());
+        var errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResponse> handleIllegalState(IllegalStateException ex) {
-        var errorResponse = new ErrorResponse("CONFLICT", ex.getMessage());
+        var errorResponse = new ErrorResponse(HttpStatus.CONFLICT.getReasonPhrase(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex) {
-        var errorResponse = new ErrorResponse("UNAUTHORIZED", ex.getMessage());
+        var errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED.getReasonPhrase(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException ex) {
-        var errorResponse = new ErrorResponse("FORBIDDEN", "Acesso negado.");
+        var errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.getReasonPhrase(), "Acesso negado.");
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
     }
 
     @ExceptionHandler(UserExceptions.InvalidCurrentPasswordException.class)
     public ResponseEntity<ErrorResponse> handleInvalidCurrentPasswordException(UserExceptions.InvalidCurrentPasswordException ex) {
-        var errorResponse = new ErrorResponse("BAD_REQUEST", ex.getMessage());
+        var errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(UserExceptions.CurrentPasswordRequiredException.class)
     public ResponseEntity<ErrorResponse> handleCurrentPasswordRequiredException(UserExceptions.CurrentPasswordRequiredException ex) {
-        var errorResponse = new ErrorResponse("BAD_REQUEST", ex.getMessage());
+        var errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
-        var errorResponse = new ErrorResponse("INTERNAL_SERVER_ERROR", "Ocorreu um erro inesperado.");
+        var errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), "Ocorreu um erro inesperado.");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
